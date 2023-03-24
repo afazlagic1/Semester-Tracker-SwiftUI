b3d44745a556959a26989c8cc9e7ec75ee87fd98
Anesa Fazlagić <xfazlag@fi.muni.cz>
2023-03-24 20:55:49 +0100

    Initial Commit

---
+++ Semester Tracker SwiftUI/Semester_Tracker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Semester_Tracker_SwiftUIApp.swift
+//  Semester Tracker SwiftUI
+//
+//  Created by Edna Fazlagić on 24. 3. 2023..
+//
+
+import SwiftUI
+
+@main
+struct Semester_Tracker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
